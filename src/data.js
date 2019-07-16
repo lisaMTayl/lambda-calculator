3383b71a4d409360a94f7a17f63b6fab966e3583
Lisa <lmkirk@gmail.com>
2019-07-16 15:51:17 -0400

    exported data.js

--- src/data.js
+++ src/data.js
@@ -6,6 +6,7 @@
 
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
 
+
 const operators = [
   {
     char: "/",
@@ -29,4 +30,7 @@ const operators = [
   }
 ];
 
+
 const specials = ["C", "x/-", "%"];
+
+export { numbers, operators, specials }
